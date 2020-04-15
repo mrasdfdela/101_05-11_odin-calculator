bfb9ae58470c9811ca8f3acb1ef5bca44d806fa5
mrasdfdela <jerryhsu830@gmail.com>
2020-04-15 15:10:13 -0400

    simple interface (w/ flexbox)

--- odin-calculator.js
+++ odin-calculator.js
@@ -1,1 +1,32 @@
-console.log("hello world!")
+buttons = [
+    { text: "Enter", type: "eval" },
+    { text: "Clear", type: "eval" },
+    { text: "0", type: "zero" },
+    { text: ".", type: "num" },
+    { text: "/", type: "operator" },
+    { text: "1", type: "num" },
+    { text: "2", type: "num" },
+    { text: "3", type: "num" },
+    { text: "x", type: "operator" },
+    { text: "4", type: "num" },
+    { text: "5", type: "num" },
+    { text: "6", type: "num" },
+    { text: "-", type: "operator" },
+    { text: "7", type: "num" },
+    { text: "8", type: "num" },
+    { text: "9", type: "num" },
+    { text: "+", type: "operator" }
+]
+
+function createBtn(buttons) {
+    var container = document.querySelector(".container")
+    buttons.forEach( button => {
+        newBtn = document.createElement("button")
+        newBtn.innerText = ` ${button.text} `
+        newBtn.classList.add(`${button.type}`)
+
+        container.appendChild(newBtn)
+    })
+}
+
+createBtn(buttons)
