a922035b011101eb45c0229c30777d0d1f949b46
mrasdfdela <jerryhsu830@gmail.com>
2020-04-20 20:08:58 -0400

    completed draft of calculator

--- odin-calculator.js
+++ odin-calculator.js
@@ -7,7 +7,7 @@ const buttons = [
     { text: "1", type: "num" },
     { text: "2", type: "num" },
     { text: "3", type: "num" },
-    { text: "x", type: "operator" },
+    { text: "*", type: "operator" },
     { text: "4", type: "num" },
     { text: "5", type: "num" },
     { text: "6", type: "num" },
@@ -18,9 +18,10 @@ const buttons = [
     { text: "+", type: "operator" }
 ]
 let savedInputs = {
-    firstNum: 1,
-    secondNum: 2,
-    operation: "+"
+    firstNum: "",
+    operation: "",
+    secondNum: "",
+    solution: true
 }
 display = document.querySelector("#display")
 
@@ -36,66 +37,103 @@ function createBtn(buttons) {
 }
 
 function inputListeners() {
-    test = document.querySelectorAll("button")
-    display = document.querySelector("#display")
+    buttonEls = document.querySelectorAll("button")
 
-    test.forEach( btn => {
+    buttonEls.forEach( btn => {
         btn.addEventListener("click", function(e) {
             e.preventDefault()
             
             input = this.innerText
-
-            switch(input) {
-                case "Clear":
-                    clear();
-                    break;
-                case "=":
-                    operate()
-                    break;
-                default:
-                    inputHandler(input)
-            }
+            input == "Clear" ? clearDisplay(true) : inputHandler(input) 
         })
     })
 }
 
 function inputHandler(input) {
-    operation = "+"
-    
-    clear(display)
-    updateDisplay(input)
+    savedInputs.firstNum == '∞' ? savedInputs.firstNum = "" :
+
+    input1 = (savedInputs.firstNum=="")
+    input2 = (savedInputs.secondNum=="")
+    input3 = (savedInputs.operation=="")
+    inputO = ('+-*/='.includes(input) )
+    solution = (savedInputs.solution == true)
+
+    if (!input3 && inputO) { // 2nd number exists & input is an operator
+        savedInputs.firstNum = operate(savedInputs)
+        savedInputs.secondNum = ""
+        input == '=' ? savedInputs.operation = "" : savedInputs.operation = input
+    } else if (!input3) { // 2nd number exists
+        savedInputs.secondNum = `${savedInputs.secondNum}${input}`
+    } else if (!input2 && input3) { // operator exists, no 2nd number
+        savedInputs.secondNum = input
+    } else if (!input1 && '+-*/'.includes(input)) { // 1st number exists & operator
+        savedInputs.operation = input
+    } else if (!input1 && input=='=') {
+        return
+    } else if (!input1 && solution) {
+        savedInputs.firstNum = input
+        savedInputs.solution = false
+    } else if (!input1) {
+        savedInputs.firstNum = `${savedInputs.firstNum}${input}`
+    } else if (input1) {
+        savedInputs.firstNum = input
+    }
+
+    updateDisplay()
+    console.log(`input: ${input}
+                n1:${savedInputs.firstNum}
+                n2:${savedInputs.secondNum}
+                op:${savedInputs.operation}
+                solution:${savedInputs.solution}`)
 }
 
-function updateDisplay(input) {
-    display.value += input
-}
+function operate(i) {
+    num1 = Number(i.firstNum)
+    num2 = Number(i.secondNum)
 
-function operate() {
-    switch(savedInputs.operation) {
+    var result
+    switch(i.operation) {
         case "+":
+            result = num1 + num2
             break
         case "-":
+            result = num1 - num2
             break
         case "*":
+            result = num1 * num2
             break
         case "/":
+            result = (num1/num2)
             break
     }
 
-    // takes in previously stored values
-    // listener with "Equals" key
-    // handle long decimals so they don't overflow
-
-    // handle when users try to divide by zero
+    i.firstNum = result
+    i.solution = true
+    updateDisplay()
+    
+    if (result==Infinity) {
+        clearDisplay(true)
+        savedInputs.solution = false
+        return "∞" // handle when users try to divide by zero
+    } else {
+        return parseFloat(result.toFixed(5)) // handle overflowing decimals
+    }
 }
 
-function clear() {
-    for (key in savedInputs) {
-        savedInputs[key] = null;
+function updateDisplay() {
+    clearDisplay()
+    i = savedInputs
+    display.value = `${i.firstNum} ${i.operation} ${i.secondNum}`
+}
+function clearDisplay(clearMemory = false) {
+    if (clearMemory == true) {
+        for (key in savedInputs) {
+            savedInputs[key] = "";
+        }
     }
     display.value = ''
 }
 
 createBtn(buttons)
 inputListeners()
-clear(display)
+clearDisplay(true)
